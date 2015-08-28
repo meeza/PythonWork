967fa15211345927af09926d77656ce9151737f8
Azeem <azeem.mohammed@xtreamit.com>
2015-08-28 22:44:24 +0530

    changes in indexer

--- SearchEngines/searchEngine.py
+++ SearchEngines/searchEngine.py
@@ -1,4 +1,8 @@
 import crawler
 
 def init_crawler():
+	global index, graph
 	index, graph = crawl_web('https://en.wikipedia.org/wiki/India')
+
+graph = {}  # <url>, [list of pages it links to]
+index = {}
